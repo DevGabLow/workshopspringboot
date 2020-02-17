8d67df09875d31201635713c367b21fa58aedcbc
gabriel falcão <gabrielfdamaceno@gmail.com>
2020-02-16 21:58:36 -0300

    Projeto criado

---
+++ src/test/java/com/gabrielfalcao/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfalcao.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
